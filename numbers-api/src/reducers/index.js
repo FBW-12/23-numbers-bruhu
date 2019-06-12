3699a17b540f0e23dd4f36c85349a04485183fe1
Bru <annabru.abh@gmail.com>
2019-06-12 15:04:57 +0200

    Reducer updated

--- numbers-api/src/reducers/index.js
+++ numbers-api/src/reducers/index.js
@@ -20,9 +20,9 @@ export function reducer(state = initialState, action) {
     case API_CALL_REQUEST:
       return { ...state, fetching: true, error: null };
     case API_CALL_SUCCESS:
-      return { ...state, fetching: false };
+      return { ...state, fetching: false, data: action.data };
     case API_CALL_FAILURE:
-      return { ...state, fetching: false };
+      return { ...state, fetching: false, data: null, error: action.error };
     default:
       return state;
   }
